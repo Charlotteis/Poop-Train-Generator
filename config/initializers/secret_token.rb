28f9cb583b89ff279c231bc08f6af40a06c479af
Charlotte Spencer <charlottelaspencer@gmail.com>
2013-07-20 14:47:56 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pooptrain::Application.config.secret_token = '4b64d73d35ecfbcd89fcff39bff7241bba39432b0090dee74e47aebc219164173c441ab046c749bef40cc88eb5a37dbe579bba562fd63d927df913ec499aeedc'
